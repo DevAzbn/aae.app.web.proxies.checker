e4bc0180d9e33d36fe7db8166365d9eac9a7515a
azbn <i@azbn.ru>
2017-05-05 09:55:10 +0300

    201705050955

---
+++ tolist.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var azbn = new require(__dirname + '/../../../../system/bootstrap')({
+	
+});
+
+var app = azbn.loadApp(module);
+
+var result = app.loadJSON('valid');
+
+var ip_arr = [];
+
+for(var i in result.items) {
+	var item = result.items[i];
+	ip_arr.push(item.proxy);
+}
+
+app.saveFile('valid.txt', ip_arr.join('\n'));
